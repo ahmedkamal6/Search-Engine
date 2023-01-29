056e332c97ab0ddfaaff899195b71414fa5cdb9d
ahmedkamal6 <87461881+ahmedkamal6@users.noreply.github.com>
2023-01-28 18:39:12 -0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using mshtml;
+using System.Net;
+
+namespace Crowler
+{
+            Queue<string> URIs = new Queue<string>();
+            Dictionary<string, bool> visited = new Dictionary<string, bool>();
+            URIs.Enqueue(URLtxt.Text);
+            int count = 0;
+            string URI = "";
+            while (count < Int32.Parse(COUNTtxt.Text))
+            {
+                try
+                {                   
+                    URI = URIs.Dequeue();
+                    WebRequest req = WebRequest.Create(URI);
+                    WebResponse res = req.GetResponse();
+                    Stream stream = res.GetResponseStream();
+                    StreamReader sr2 = new StreamReader(stream);
+                    string rString = sr2.ReadToEnd();
+                    stream.Close();
+                    sr2.Close();
+                    res.Close();
+                    IHTMLDocument2 doc = new HTMLDocumentClass();
+                    doc.write(rString);
+                    IHTMLElementCollection elements = doc.all;
+                    IHTMLElementCollection elems = doc.links;
+                    bool english = false;
+                    foreach (IHTMLElement el in elements)
+                    {
+                        if (el.tagName == "HTML")
+                        {
+                            string Lang = (string)el.getAttribute("lang", 0);
+                            if (Lang == "en")
+                                english = true;
+                        }
+                    }
+                    if (!english)
+                        continue;
+                    foreach (IHTMLElement el in elems)
+                    {
+                        string link = (string)el.getAttribute("href", 0);
+                        if (!visited.ContainsKey(link) && link.StartsWith("https"))
+                        {
+                            URIs.Enqueue(link);
+                            visited.Add(link, false);
+                        }
+                    }
+                    
+                    count++;
+                }
+                catch (Exception a)
+                {
+                    Console.WriteLine(a.ToString());
+                    continue;
+                }
+            }
+
+}
